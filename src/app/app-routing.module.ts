6cf454fd32423b2a8bacc6d6502d11b36aa9fd47
sergio <pugliese.sergio1987@hotmail.it>
2025-01-12 15:56:03 +0100

    Add Firebase configuration and setup for hosting
    - Created .firebaserc and firebase.json for Firebase project configuration.
    - Updated angular.json to include Firebase analytics.
    - Added Firebase SDK to public/index.html for hosting.
    - Implemented Firebase initialization in app-routing.module.ts.
    - Created GitHub Actions workflows for automatic deployment on merge and pull requests.
    - Updated package.json and package-lock.json to include Firebase dependencies.
    - Added firebase.config.ts for Firebase configuration management.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYXphliWB4Jnz9CEYGLejJU8i-gd0fzTc',
+  authDomain: 'checklist-porte-tagliafuoco.firebaseapp.com',
+  projectId: 'checklist-porte-tagliafuoco',
+  storageBucket: 'checklist-porte-tagliafuoco.firebasestorage.app',
+  messagingSenderId: '309733437254',
+  appId: '1:309733437254:web:2d56d98bca0bf2ff05ff47',
+  measurementId: 'G-P8G9F5HHW3',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,20 @@
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyCYXphliWB4Jnz9CEYGLejJU8i-gd0fzTc',
+//   authDomain: 'checklist-porte-tagliafuoco.firebaseapp.com',
+//   projectId: 'checklist-porte-tagliafuoco',
+//   storageBucket: 'checklist-porte-tagliafuoco.appspot.com',
+//   messagingSenderId: '309733437254',
+//   appId: '1:309733437254:web:2d56d98bca0bf2ff05ff47',
+//   measurementId: 'G-P8G9F5HHW3',
+// };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
